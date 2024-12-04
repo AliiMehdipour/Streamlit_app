bbc8eb6a14f05458e4a5c9618608754be60bbe2f
AliiMehdipour <69368811+AliiMehdipour@users.noreply.github.com>
2024-12-04 14:20:34 +0100

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -11,7 +11,7 @@ st.set_page_config(page_title="Dashboard", layout="wide")
 @st.cache_resource
 def initialize_database_connection():
     # MongoDB connection
-    client = MongoClient('mongodb+srv://$DB_USERNAME:$DB_TOKEN@cluster0.2kwup.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+    client = MongoClient('mongodb+srv://alimehdipourka:diI4xvdIelYlNcl6@cluster0.2kwup.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
     db = client['Job']
     collection = db['jobs']
     return collection
